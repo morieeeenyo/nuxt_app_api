16844ed2dbf40f25a02fa1d245cb1ae4c3b3b785
togo-mentor <uint2435@gmail.com>
2021-12-25 16:19:09 +0900

    emailのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,10 @@ class User < ApplicationRecord
   # validates
   validates :name, presence: true, length: { maximum: 30, allow_nil: true }
 
+  VALID_EMAIL_REGEX = /\A\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*\z/.freeze
+  validates :email, presence: true, email: { allow_blank: true }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, uniqueness: { scope: :activated } # ステータスの有効・無効に対してemailは一意
+
   VALID_PASSWORD_REGEX = /\A[\w\-]+\z/.freeze # 半角英数もしくは-_
   validates :password, presence: true,
                        length: { minimum: 8 },
@@ -17,4 +21,14 @@ class User < ApplicationRecord
                          message: :invalid_password
                        },
                        allow_nil: true
+
+  before_validation :downcase_email
+
+  private
+
+  # email小文字化
+  def downcase_email
+    self.email.downcase! if email
+  end
+
 end
