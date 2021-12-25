9b377fc88488e94c6d234fe675335a6fcc247cdc
togo-mentor <uint2435@gmail.com>
2021-12-25 14:35:15 +0900

    バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+  # bcryptの機能でパスワード暗号化してくれる
+  # 保存先はpassword_digest
+  # password, password_confirmationでパスワード判定
+  # authenticateメソッドでパスワードの一致判定を行うことができる
+
+  has_secure_password
+
+  # validates
+  validates :name, presence: true, length: { maximum: 30, allow_nil: true }
+
+  VALID_PASSWORD_REGEX = /\A[\w\-]+\z/.freeze # 半角英数もしくは-_
+  validates :password, presence: true,
+                       length: { minimum: 8 },
+                       format: {
+                         with: VALID_PASSWORD_REGEX
+                       },
+                       allow_nil: true
 end
